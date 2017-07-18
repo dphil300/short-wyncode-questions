1172292fc8fb530d6bbff0fa21d14c3380d0ed21
Daniel Philpot <Dphil300@gmail.com>
2017-07-18 18:55:36 -0400

    wyncode questions from homework

---
+++ short_wyncode_questions.rb
@@ -0,0 +1,8 @@
+question: What is an instance and how do you remember?
+  answer: An instance is a new creation of a particular class,
+  how I remember is by using the word "instant" and knowing thats how fast it
+  takes to create something
+
+question:Why do you want to code?
+  answer: Because I want to change the world. The world is full of problems and
+  coding is just problem solving in various ways
